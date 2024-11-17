26564b6cb73c4e903a7503a2fc08e014c5642480
Daniel Hsieh <dan2y006@gmail.com>
2024-11-17 20:43:38 +0800

    feat: implement video navigation controls for backward and forward playback

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -152,8 +152,48 @@ watch([scrubbing, elementX], async () => {
     videoCurrentTime.value = newStartTime - curClip.start
     await onClickTimestamp(curClip)
     videoRef.value.currentTime = videoCurrentTime.value
+    videoRef.value.pause()
+    playing.value = false
   }
 })
+
+async function onClickBackward() {
+  if (videoCurrentTime.value > 0) {
+    videoCurrentTime.value = 0
+    videoRef.value.currentTime = 0
+  } else {
+    const currentClipIdx = highlightedClips.value.findIndex(
+      (clip) => clip.start === currentClip.value.start
+    )
+    const prevClipIdx = currentClipIdx - 1
+    if (prevClipIdx >= 0) {
+      const prevClip = highlightedClips.value[prevClipIdx]
+      await onClickTimestamp(prevClip)
+    } else {
+      await onClickTimestamp(highlightedClips.value?.[0] || currentClip.value)
+    }
+  }
+  videoRef.value.pause()
+  playing.value = false
+}
+
+async function onClickForward() {
+  const currentClipIdx = highlightedClips.value.findIndex(
+    (clip) => clip.start === currentClip.value.start
+  )
+  const nextClipIdx = currentClipIdx + 1
+  if (nextClipIdx < highlightedClips.value.length) {
+    const nextClip = highlightedClips.value[nextClipIdx]
+    await onClickTimestamp(nextClip)
+  } else {
+    await onClickTimestamp(
+      highlightedClips.value?.[highlightedClips.value.length - 1] ||
+        currentClip.value
+    )
+  }
+  videoRef.value.pause()
+  playing.value = false
+}
 </script>
 
 <template>
@@ -166,9 +206,9 @@ watch([scrubbing, elementX], async () => {
   </div>
   <div
     v-else
-    class="w-full md:h-[100dvh] flex items-stretch gap-0 md:flex-nowrap flex-wrap md:!flex-row flex-col-reverse"
+    class="w-full md:h-[100dvh] flex items-stretch gap-0 md:flex-nowrap flex-wrap"
   >
-    <div class="bg-[#efefef] p-4 basis-full md:basis-1/2">
+    <div class="bg-[#efefef] p-4 basis-full md:basis-1/2 order-2 md:order-1">
       <h3 class="text-xl font-bold !mb-5">Transcript</h3>
       <div
         v-for="(section, sectionIdx) in video.sections || []"
@@ -182,7 +222,7 @@ watch([scrubbing, elementX], async () => {
           :class="{
             '!bg-blue-500': clip.highlight,
             '!text-white': clip.highlight,
-            'border-2 border-yellow-500': currentClip.start === clip.start
+            'border-2 border-red-500': currentClip.start === clip.start
           }"
           class="bg-white p-2 flex gap-2 !mb-2 rounded cursor-pointer"
           @click="onClickClip(clip)"
@@ -205,15 +245,16 @@ watch([scrubbing, elementX], async () => {
         </div>
       </div>
     </div>
-    <div class="bg-gray-800 p-4 basis-full md:basis-1/2">
+    <div
+      class="bg-gray-800 p-4 basis-full md:basis-1/2 pb-6 order-1 md:order-2"
+    >
       <h3 class="text-xl font-bold !mb-3 text-white">Preview</h3>
-      <div class="flex justify-center">
+      <div class="flex relative justify-center w-full aspect-video">
         <video
           ref="videoRef"
           :src="currentClip.url"
-          preload="auto"
           playsinline
-          class="w-auto mx-auto"
+          class="block"
           :key="currentClip.url"
           @timeupdate="onVideoTimeUpdate"
           @ended="onVideoEnded"
@@ -227,7 +268,7 @@ watch([scrubbing, elementX], async () => {
           />
         </video>
       </div>
-      <div class="w-full !mt-2">
+      <div class="w-full !mt-2 relative">
         <div
           class="flex justify-between items-center p-2 !pb-4 [&>.p-button]:text-white [&>.p-button:hover]:!text-black"
         >
@@ -237,6 +278,7 @@ watch([scrubbing, elementX], async () => {
             variant="text"
             rounded
             aria-label="Backward"
+            @click="onClickBackward"
           />
           <Button
             :icon="playing ? 'pi pi-pause' : 'pi pi-play'"
@@ -252,16 +294,17 @@ watch([scrubbing, elementX], async () => {
             variant="text"
             rounded
             aria-label="Forward"
+            @click="onClickForward"
           />
           <span class="text-white">
             {{
-              Math.floor(currentClip.start / 60)
+              Math.floor((currentClip.start + videoCurrentTime) / 60)
                 .toString()
                 .padStart(2, '0')
             }}
             :
             {{
-              Math.floor(currentClip.start % 60)
+              Math.floor((currentClip.start + videoCurrentTime) % 60)
                 .toString()
                 .padStart(2, '0')
             }}
