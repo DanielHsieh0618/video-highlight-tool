7d419202fcc6ea49adf602af5b6dd33707b05e7f
Daniel Hsieh <dan2y006@gmail.com>
2024-11-17 08:53:32 +0800

    feat: update catch-all route to use named route for better clarity

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/EditorView.vue')
     },
-    { path: '*', component: UploadView }
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: UploadView }
   ]
 })
 
